6f3ba0d87a51860225328cf4d36eb1efa4594dd3
liyche <liyche@umich.edu>
2022-09-05 22:30:25 -0400

    add script to clear output files

---
+++ clear_output.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# WARNING: DO NOT RUN THIS SCRIPT BEFORE BACKING UP IF YOU WANT TO KEEP ANY OF THE OUTPUT
+
+# How to Run:
+#   chmod +x clear_output.sh
+#   ./clear_output.sh
+
+rm -rf FILES_OUT/*
+rm -rf JJOGAEGI_OUT/*
+rm -f words.txt
+rm -f my_learning_book.csv
